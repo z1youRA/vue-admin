015d09506053f045f9d6effa9c948e595c158bb8
z1youra <374671110@qq.com>
2023-02-28 21:27:40 +0800

    ✨ feat: 增加向不同页面的路由

--- src/layout/LayoutPage.vue
+++ src/layout/LayoutPage.vue
@@ -1,12 +1,12 @@
 <template>
   <div class="app-wrapper">
     <el-container>
-      <el-aside width="200px">
+      <el-aside width="200px" class="sidebar-container">
         <Menu />
       </el-aside>
       <el-container>
         <el-header>Header</el-header>
-        <el-main>Main</el-main>
+        <el-main><router-view /></el-main>
       </el-container>
     </el-container>
   </div>
--- src/router/index.js
+++ src/router/index.js
@@ -4,7 +4,50 @@ const routes = [
   {
     path: '/',
     name: '/',
-    component: () => import('../layout/LayoutPage.vue')
+    component: () => import('../layout/LayoutPage.vue'),
+    redirect: '/users',
+    children: [
+      {
+        path: 'users',
+        name: 'users',
+        component: () => import('@/views/users/UsersPage.vue')
+      },
+      {
+        path: 'categories',
+        name: 'categories',
+        component: () => import('@/views/categories/CategoriesPage.vue')
+      },
+      {
+        path: 'goods',
+        name: 'goods',
+        component: () => import('@/views/goods/GoodsPage.vue')
+      },
+      {
+        path: 'orders',
+        name: 'orders',
+        component: () => import('@/views/orders/OrdersPage.vue')
+      },
+      {
+        path: 'params',
+        name: 'params',
+        component: () => import('@/views/params/ParamsPage.vue')
+      },
+      {
+        path: 'reports',
+        name: 'reports',
+        component: () => import('@/views/reports/ReportsPage.vue')
+      },
+      {
+        path: 'rights',
+        name: 'rights',
+        component: () => import('@/views/rights/RightsPage.vue')
+      },
+      {
+        path: 'roles',
+        name: 'roles',
+        component: () => import('@/views/roles/RolesPage.vue')
+      }
+    ]
   },
   {
     path: '/login',

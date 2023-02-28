f96e6ae8bbf126671e998169c68ec9d25be570b8
z1youra <374671110@qq.com>
2023-02-28 15:33:18 +0800

    ✨ feat: 添加请求拦截器
    在request报文中加入token信息

--- src/api/request.js
+++ src/api/request.js
@@ -6,6 +6,16 @@ const service = axios.create({
   timeout: 5000
 })
 
+//在request中插入token信息
+service.interceptors.request.use(
+  (config) => {
+    config.headers.Authorization = localStorage.getItem('token')
+    return config
+  },
+  (error) => {
+    return Promise.reject(new Error(error))
+  }
+)
 service.interceptors.response.use(
   (response) => {
     const { meta, data } = response.data
